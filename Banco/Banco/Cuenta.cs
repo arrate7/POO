598861c7747551b78551366a3eea39a23503d9d0
arrate7 <43437355+arrate7@users.noreply.github.com>
2018-10-14 23:32:53 +0200

    .

---
+++ Banco/Banco/Cuenta.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    class Cuenta
+    {
+        //atributos son private para que solo sean accedidos desde 
+        //los métodos de la clase
+
+        private String nombre;
+        private String nombreCuenta;
+        private double saldo;
+        private double tipoInteres;
+
+        //constructor por defecto( no hace falta escribirlo)
+        public Cuenta() {
+
+        }
+
+        //contructor con parámetros
+        public Cuenta(String nombre, String cuenta, double saldo, double tipo)
+        {
+            this.nombre=nombre;
+            this.nombreCuenta=cuenta;
+            this.saldo=saldo;
+            this.tipoInteres=tipo;
+        }
+
+
+
+        //métodos get y set
+
+        public String GetNombre() {
+
+            return nombre;
+
+        }
+        public void SetNombre(String nombre)
+        {
+
+            this.nombre=nombre;
+
+        }
+
+
+        public String GetNombreCuenta()
+        {
+
+            return nombreCuenta;
+
+        }
+        public void SetNombreCuenta(String nombreCuenta)
+        {
+
+            this.nombreCuenta = nombreCuenta;
+
+        }
+
+
+        public double GetSaldo()
+        {
+
+            return saldo;
+
+        }
+        public void SetSaldo(double saldo)
+        {
+
+            this.saldo = saldo;
+
+        }
+
+        public double GetTipoInteres()
+        {
+
+            return tipoInteres;
+
+        }
+        public void SetTipoInteres(double tipo)
+        {
+
+            this.tipoInteres = tipo;
+
+        }
+        
+
+
+        //métodos
+  
+
+        public void Ingreso(double cantidad)
+        {
+
+            if (cantidad < 0)
+            {
+                Console.WriteLine("error, cantidad negativa...");
+                Console.ReadLine();
+                return;
+            }
+            saldo = saldo + cantidad;
+
+        }
+
+        public void Reintegro(double cantidad)
+        {
+
+            if (saldo - cantidad < 0)
+            {
+                Console.WriteLine("Error: no dispone de saldo ...");
+                Console.ReadLine();
+                return;
+            }
+            saldo = saldo - cantidad;
+
+        }
+    }
+}
---
+++ Banco/Banco/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //crear objetos sin parametros
+            Cuenta c1 = new Cuenta();
+            c1.SetNombre("Arrate");
+            c1.SetNombreCuenta("Cuenta Bancaria de Arrate");
+            c1.SetSaldo(60000);
+            c1.SetTipoInteres(2.2);
+
+
+            //Crear un objeto mediante parametros
+            Cuenta c2 = new Cuenta("Jose", "Cuenta Bancaria de Jose", 50000, 2.5);
+
+            Console.WriteLine("Dinero en la cuenta de " + c1.GetNombre() + ":" + c1.GetSaldo());
+            //ingresamos 10000 en la cuenta
+            c1.Ingreso(10000);
+            Console.WriteLine("Dinero en la cuenta de " + c1.GetNombre() + " con el ingreso:" + c1.GetSaldo());
+
+            Console.WriteLine("Dinero en la cuenta de " + c2.GetNombre() + ":" + c2.GetSaldo());
+            //quitamos 10000 en la cuenta
+            c2.Reintegro(10000);
+            Console.WriteLine("Dinero en la cuenta de " + c2.GetNombre() + "con el reintegro:" + c2.GetSaldo());
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
